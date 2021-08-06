e2e686822227a23a63233aee69fd1aa4d03f27ef
Bukola Omojowo <bomojowo@gmail.com>
2021-08-06 11:49:24 +0100

    connected front end to API

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
+import { InputPaste } from "./components/InputPaste";
 
 function App() {
+  const [data, setData] = useState<string>()
+  const [paste, setPaste] = useState<string>()
+
+  //fetching form API
+  const loadData = async () => {
+    const apiBaseURL = process.env.REACT_APP_API_BASE;
+    const response = await fetch(apiBaseURL + "/");
+    const body = await response.json();
+    setData(body.pastes[1].pasted_text)
+    console.log(body.pastes[1].pasted_text);
+  };
+  
+
+  function storePaste (){
+    console.log('testing the store button')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Make changes <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Paste Bin</h1>
+      <input placeholder="Paste your code" />
+      <button onClick={storePaste}>Save Paste</button>
+      <hr />
+      <button onClick={loadData}>Get Pastes</button>
+      <h3>GET pastes</h3>
+      {data}
+      <InputPaste />
+      
     </div>
   );
 }
