13365f0ded58d1b29e91a307627324f58845e561
Bukola Omojowo <bomojowo@gmail.com>
2021-08-09 11:59:26 +0100

    removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import React, { useState } from "react";
+//import React, { useState } from "react";
 import "./App.css";
 import { InputPaste } from "./components/InputPaste";
 import { GetPastes } from "./components/GetPastes";
 function App() {
-  const [data, setData] = useState<string[]>([]);
-  // const [code, setCode] = useState<string>()
+ 
+  
 
-  //fetching form API
-  const loadData = async () => {
-    const apiBaseURL = process.env.REACT_APP_API_BASE;
-    const response = await fetch(apiBaseURL + "/");
-    const body = await response.json();
-    setData([...data, body.pastes[0].description]);
-    setData(body.pastes[1].pasted_text);
-    console.log(body.pastes[0]);
-  };
+  
 
   return (
     <div className="App">
@@ -22,9 +14,9 @@ function App() {
       <InputPaste />
       <hr />
       <h3>GET previous pastes</h3>
-      {<button onClick={loadData}>Get Pastes</button>}
+      
       <br />
-      {data}
+     <GetPastes/>
     </div>
   );
 }
