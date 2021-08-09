7cd76dba93b5678c54a1949f61f738781846d597
Bukola Omojowo <bomojowo@gmail.com>
2021-08-09 18:03:27 +0100

    created delete paste function

--- src/components/GetPastes.tsx
+++ src/components/GetPastes.tsx
@@ -25,6 +25,16 @@ export function GetPastes(): JSX.Element {
     alert('You have clicked this post')
   }
 
+  async function handlePostDelete(id: number){
+    //deletes post
+    const apiBaseURL = process.env.REACT_APP_API_BASE;
+    const deletePost = await fetch(apiBaseURL + `/pastes/${id}`,{
+      method: "DELETE",
+    })
+    setPastes(pastes.filter((pastes) => pastes.id !== id))
+  }
+
+ 
   return (
     <div>
       {
@@ -36,7 +46,7 @@ export function GetPastes(): JSX.Element {
       <div>
         {pastes &&
           pastes.map((paste) => (
-            <div className="get-pastes" key={paste.id} onClick={handlePostClick}>
+            <div className="get-pastes" key={paste.id}>
               <b>username:</b>
               {paste.user_name}
               <br />
@@ -45,9 +55,14 @@ export function GetPastes(): JSX.Element {
               <br />
               <b>code:</b>
               {paste.code}
+              <br/>
+              <button onClick={handlePostClick}>Show More</button>
+              <button className="edit-btn">Edit Paste</button>
+              <button className="delete-btn" onClick={() => handlePostDelete(paste.id)}>Delete</button>
             </div>
           ))}
       </div>
+      
     </div>
   );
 }
