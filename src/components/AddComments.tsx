753a7a8b8624e9bf403e949dfa3480201a7bc728
Bukola Omojowo <bomojowo@gmail.com>
2021-08-13 09:53:14 +0100

    created AddCompents component

--- src/components/AddComments.tsx
+++ src/components/AddComments.tsx
@@ -1,3 +1,37 @@
-export function AddComments() {
-  return <div>hi</div>;
+import { getPastesProps } from "./GetPastes"
+// import { Modal, Button } from "react-bootstrap"
+// import {useState} from 'react'
+
+interface AddCommentsProps {
+    paste: getPastesProps;
+  } 
+
+export function AddComments({paste}: AddCommentsProps):JSX.Element {
+
+    // const [code, setCode] = useState<string>("");
+    // const [show, setShow] = useState(false);
+
+    // function handleClose() {
+    //     setShow(false);
+    //     setCode(code)
+    //   }
+
+    //   function handleShow() {
+    //     setShow(true);
+    //   }
+    
+    function handleComment(){
+    console.log('testing comment function')
+    }
+
+    //postcomment function
+
+    //getcomment function
+
+    //deletecomment function
+
+
+  return (
+      <button onClick={() => handleComment()}>Comment</button>
+  )
 }
