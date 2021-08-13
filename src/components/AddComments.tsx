504937ed1893f2ce808baf3c60198b33c072de48
Bukola Omojowo <bomojowo@gmail.com>
2021-08-13 11:37:19 +0100

    added modal box for AddComments component

--- src/components/AddComments.tsx
+++ src/components/AddComments.tsx
@@ -1,37 +1,86 @@
-import { getPastesProps } from "./GetPastes"
-// import { Modal, Button } from "react-bootstrap"
-// import {useState} from 'react'
+//import { getPastesProps } from "./GetPastes"
+import { Modal, Button } from "react-bootstrap"
+import {useState} from 'react'
 
-interface AddCommentsProps {
-    paste: getPastesProps;
-  } 
+export interface AddCommentsProps {
+   comment: string
+  }
 
-export function AddComments({paste}: AddCommentsProps):JSX.Element {
+export function AddComments():JSX.Element {
+    const [showComments, setShowComments] = useState<AddCommentsProps[]>([]);
 
-    // const [code, setCode] = useState<string>("");
-    // const [show, setShow] = useState(false);
+    const [show, setShow] = useState(false);
 
-    // function handleClose() {
-    //     setShow(false);
-    //     setCode(code)
-    //   }
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
-    //   function handleShow() {
-    //     setShow(true);
-    //   }
-    
-    function handleComment(){
-    console.log('testing comment function')
+//getcomment function
+  async function getComments() {
+    const apiBaseURL = process.env.REACT_APP_API_BASE;
+    const response = await fetch(apiBaseURL + "/pastes/:paste_id/comments", {
+      method: "GET",
+    });
+    const body = await response.json();
+    setShowComments(body.showComments);
+  }
+
+  function handleSaveComment(){
+      handleClose()
+    console.log('Comment saved')
+    }
+
+    function handleAddComment() {
+        console.log('Comment Added')
     }
 
     //postcomment function
 
-    //getcomment function
+    
 
     //deletecomment function
 
 
   return (
-      <button onClick={() => handleComment()}>Comment</button>
-  )
+    <>
+    {/* <button onClick={() => handleComment()}>Comment</button> */}
+      <Button variant="primary" onClick={handleShow}>
+        See comments
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Comments</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            Add comment: <input/>
+            <button onClick={handleAddComment}>Add comment</button>
+            <hr/>
+            <button onClick={getComments}>Show Previous Comments</button>
+            <br/>
+            
+            <ul>
+                <li>
+                    Able to delete a comment
+                </li>
+            </ul>
+            {showComments && showComments.map((comment) => (
+                <div>
+                    <b>comment:</b>
+                    {comment.comment}
+                </div>
+            ))}
+            
+            </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleSaveComment}>
+            Save Comment
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+
 }
